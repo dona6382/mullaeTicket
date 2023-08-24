a67c6cdb9557a8acc948448cf49ba711ad2d58df
dona6382 <46666529+dona6382@users.noreply.github.com>
2023-08-24 15:24:35 +0900

    로직 정리 커밋

--- app.js
+++ app.js
@@ -1,15 +1,14 @@
 const axios = require('axios');
 
 
-// cookie = sales_code, unit_price만 변경해주면 완료
-
 function settingOptions(){
     const url = 'http://online122.youth.seoul.kr/s_center/ajax.reservation_proc.php';
 
-    const salesCode = '00100300045';
+    //강의에 따른 변경
+    const salesCode = '00100300077';
     const unitPrice = '47300';
     const cookie = 'PHPSESSID='
-        + 'cgofola3bkgig6448mrt69u27q'
+        + '4m3su2khmlkd1jnufpjfcartrk'
         + '; _ga=GA1.1.1908262919.1689579664; 342f86b2297eed1b3df8f15ce621c527=a2ltNjM4MjYzODI%3D; _ga_8Z1PVHFSHW=GS1.1.1690241638.4.1.1690241814.0.0.0';
 
     const headers = {
@@ -18,7 +17,6 @@ function settingOptions(){
         'Accept-Language': 'ko-KR,ko;q=0.9,en-US;q=0.8,en;q=0.7',
         'Connection': 'keep-alive',
         'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
-        // 'Cookie': 'PHPSESSID=cgofola3bkgig6448mrt69u27q; _ga=GA1.1.1908262919.1689579664; 342f86b2297eed1b3df8f15ce621c527=a2ltNjM4MjYzODI%3D; _ga_8Z1PVHFSHW=GS1.1.1690241638.4.1.1690241814.0.0.0',
         'Cookie': cookie,
         'Host': 'online122.youth.seoul.kr',
         'Origin': 'http://online122.youth.seoul.kr',
@@ -28,9 +26,7 @@ function settingOptions(){
     };
 
     const data = new URLSearchParams();
-    // data.append('sales_code', '00100300045');
     data.append('sales_code', salesCode);
-    // data.append('unit_price', '47300');
     data.append('unit_price', unitPrice);
     data.append('first_start_day_yn', 'Y');
     data.append('monthqty', '01');
